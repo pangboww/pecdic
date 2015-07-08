52edb5b305644bffe5febc5863b5b1357b5a8408
pangboww <pangboww@gmail.com>
2015-07-08 14:18:21 -0300

    add adding word

--- client/templates/words/add_word.js
+++ client/templates/words/add_word.js
@@ -1,3 +1,36 @@
-/**
- * Created by bopang on 07/07/15.
- */
+Template.addWord.onCreated(function() {
+    Session.set('addWordErrors', {});
+});
+
+Template.addWord.helpers({
+    errorMessage: function(field) {
+        return Session.get('addWordErrors')[field];
+    },
+    errorClass: function (field) {
+        return !!Session.get('addWordErrors')[field] ? 'has-error' : '';
+    }
+});
+Template.addWord.events({
+    'submit form': function(e){
+        e.preventDefault();
+
+        var word = {
+            portuguese: $(e.target).find('[name=portuguese]').val(),
+            english: $(e.target).find('[name=english]').val(),
+            chinese: $(e.target).find('[name=chinese]').val()
+        };
+
+        var errors = validateWord(word);
+        if (errors.portuguese || errors.english || errors.chinese)
+            return Session.set('addWordErrors', errors);
+
+        Meteor.call('wordInsert', word, function(error, result) {
+            if (error)
+                return throwError(error.reason);
+
+            Router.go('home');
+        });
+
+    }
+});
+
